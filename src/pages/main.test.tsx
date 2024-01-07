f1acda2d43d7bf186af33ba44013582b75b03ffe
rivailJunior <rivail.rj@gmail.com>
2024-01-07 01:06:05 -0400

    feat: adding test for length of list itens

--- src/pages/main.test.tsx
+++ src/pages/main.test.tsx
@@ -28,8 +28,9 @@ describe("Main", () => {
   });
 
   test("should render App with input and label and list with one child", async () => {
-    httpClientMock.get.mockImplementation(() => Promise.resolve(expectedData));
-
+    httpClientMock.get.mockImplementation(() =>
+      Promise.resolve([expectedData[0]])
+    );
     render(<App countryDao={countryDao} />);
 
     // eslint-disable-next-line testing-library/no-unnecessary-act
@@ -42,7 +43,27 @@ describe("Main", () => {
       );
     });
 
-    expect(await screen.findByTestId("countryList")).toBeInTheDocument();
+    const countries = await screen.findByTestId("countryList");
+    expect(countries).toBeInTheDocument();
     expect(await screen.findByDisplayValue(countryName)).toBeInTheDocument();
+    expect(countries?.childNodes).toHaveLength(1);
+  });
+
+  test("should render App with input and label and list with 3 childs", async () => {
+    httpClientMock.get.mockImplementation(() => Promise.resolve(expectedData));
+    render(<App countryDao={countryDao} />);
+    // eslint-disable-next-line testing-library/no-unnecessary-act
+    await act(() => {
+      fireEvent.change(
+        screen.getByRole("textbox", { name: "Search Country" }),
+        {
+          target: { value: "Bra" },
+        }
+      );
+    });
+
+    const countries = await screen.findByTestId("countryList");
+    expect(countries).toBeInTheDocument();
+    expect(countries?.childNodes).toHaveLength(3);
   });
 });
